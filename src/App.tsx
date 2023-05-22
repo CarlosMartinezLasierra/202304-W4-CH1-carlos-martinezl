923b0cd713c3b44c152e961fa92d413250f3dae1
Carlos Martinez <carlosmartinezlasierra@gmail.com>
2023-05-22 20:46:11 +0200

    Understanding code

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/Button";
+import { Gentleman } from "./components/Gentleman";
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Button></Button>
+      <Gentleman></Gentleman>
+    </>
+  );
+}
+
+export default App;
