ad8a1e559041d3f8be389a525d5302315a4ab207
Carlos Martinez <carlosmartinezlasierra@gmail.com>
2023-05-22 18:07:16 +0200

    Add properties to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Header } from './assets/components/header'
+import React { useState } from "react";
+import Gentleman from "./Gentleman";
+
+const gentlemenData = [
+  {
+    id: 1,
+    name: "Bertin Osborne",
+    status: "Alive",
+    profession: "Youtuber",
+    twitter: "@osbourne",
+    picture: "bertin.jpg",
+    alternativeText: "Osbourne pointing at you",
+    selected: true,
+  },
+  {
+    name: "The Farytale",
+    status: "RIP",
+    profession: "Influencer",
+    twitter: "pending",
+    picture: "fary.jpg",
+    alternativeText: "The Fary pointing at you",
+    id: 2,
+    selected: false,
+  },
+  {
+    id: 3,
+    name: "Julius Churchs",
+    status: "Alive",
+    profession: "Java developer",
+    twitter: "@julius_churchs",
+    picture: "julio.jpg",
+    alternativeText: "Churchs pointing at you",
+    selected: true,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gentlemen, setGentlemen] = useState(gentlemenData);
+
+  const handleSelectAll = () => {
+    const updatedGentlemen = gentlemen.map((gentleman) => ({
+      ...gentleman,
+      selected: true,
+    }));
+    setGentlemen(updatedGentlemen);
+  };
 
   return (
-    <>
-      <div>
-        <Header></Header>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="main">
+        <div className="main-header">
+          <h1 className="main-title">Gentlemen Collection</h1>
+          <Button onClick={handleSelectAll}>Select All</Button>
+        </div>
+        <div className="gentlemen-list">
+          {gentlemen.map((gentleman) => (
+            <Gentleman key={gentleman.id} gentleman={gentleman} />
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
